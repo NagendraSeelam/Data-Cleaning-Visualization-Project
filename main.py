cabb3b0d97da5de21dd9a41cb226b459c1640cf1
NagendraSeelam <nagendraseelam5555@gmail.com>
2026-06-08 22:22:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Data Cleaning and Visualization Project")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = {
+    "Category": ["Movies", "TV Shows"],
+    "Count": [6131, 2676]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+plt.bar(df["Category"], df["Count"])
+plt.title("Movies vs TV Shows")
+plt.xlabel("Category")
+plt.ylabel("Count")
+plt.savefig("movies_vs_tvshows.png")
+
+print("Visualization created successfully")
